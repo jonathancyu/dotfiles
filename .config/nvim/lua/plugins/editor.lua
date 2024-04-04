4dba8c0d98f1d3afb6c6ef06e730145ac78ff4d4
Jonathan Yu <yucjonathan@gmail.com>
2024-04-04 01:05:58 -0500

    fixed missing configs

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -7,7 +7,17 @@ return {
     {
         'theprimeagen/harpoon', 
         config = function(plugin)
-            require('config/harpoon')
+            local mark = require('harpoon.mark')
+            local ui = require('harpoon.ui')
+
+            vim.keymap.set('n', '<leader>a', mark.add_file)
+            vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+
+
+            vim.keymap.set('n', '<C-j>', function() ui.nav_file(1) end)
+            vim.keymap.set('n', '<C-k>', function() ui.nav_file(2) end)
+            vim.keymap.set('n', '<C-l>', function() ui.nav_file(3) end)
+            vim.keymap.set('n', '<C-;>', function() ui.nav_file(4) end)
         end
     },
 
@@ -20,7 +30,12 @@ return {
         -- or                            , branch = '0.1.x',
         dependencies = { {'nvim-lua/plenary.nvim'} },
         config = function(plugin)
-            require('config/telescope')
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>ps', function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") });
+            end)
         end
     },
 
