bda172d9607faed10fd2ad8b49e0b6aa4cc71a94
Jonathan Yu <yucjonathan@gmail.com>
2024-04-15 19:50:10 -0500

    fixed colorscheme priority

--- .config/nvim/lua/plugins/appearance/colorscheme.lua
+++ .config/nvim/lua/plugins/appearance/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
     'rebelot/kanagawa.nvim',
     name = 'kanagawa',
+    priority = 100,
     config = function()
         require('kanagawa').load('wave')
         vim.opt.termguicolors = true
