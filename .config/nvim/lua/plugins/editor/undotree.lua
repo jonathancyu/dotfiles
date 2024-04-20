1dd8083fbdbf3aaa1fdb11d663cac1ba04dc5f35
Jonathan Yu <yucjonathan@gmail.com>
2024-04-20 18:20:35 -0500

    fixed undotree

--- .config/nvim/lua/plugins/editor/undotree.lua
+++ .config/nvim/lua/plugins/editor/undotree.lua
@@ -1,12 +1,13 @@
 return {
     'mbbill/undotree',
     branch = '*',
+    lazy = false,
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons',
         'MunifTanjim/nui.nvim',
     },
     keys = {
-        { '<leader>u', vim.cmd.UndoTreeToggle, desc = 'Toggle undotree' }
+        { '<leader>u', vim.cmd.UndotreeToggle, desc = 'Toggle undotree' }
     }
 }
