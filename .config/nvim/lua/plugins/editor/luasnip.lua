e2ff60e66939f3aa43eea7dd973774459bcece5b
Jonathan Yu <yucjonathan@gmail.com>
2024-04-24 10:50:31 -0500

    updated font, removed window decorations

--- .config/nvim/lua/plugins/editor/luasnip.lua
+++ .config/nvim/lua/plugins/editor/luasnip.lua
@@ -1,17 +1,28 @@
 return {
-    -- TODO: redo this
-    --
-    -- 'L3MON4D3/LuaSnip',
-    -- config = function()
-    --     local ls = require('luasnip')
-    --     vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-    --     vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
-    --     vim.keymap.set({"i", "s"}, "<C-H>", function() ls.jump(-1) end, {silent = true})
-    --
-    --     vim.keymap.set({"i", "s"}, "<C-E>", function()
-    --         if ls.choice_active() then
-    --             ls.change_choice(1)
-    --         end
-    --     end, {silent = true})
-    -- end
+    {
+        'L3MON4D3/LuaSnip',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+        config = function()
+            local ls = require('luasnip')
+            -- TODO: make lazy
+            -- TODO: add java class with package here
+            vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+            vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+            vim.keymap.set({"i", "s"}, "<C-H>", function() ls.jump(-1) end, {silent = true})
+
+            vim.keymap.set({"i", "s"}, "<C-E>", function()
+                if ls.choice_active() then
+                    ls.change_choice(1)
+                end
+            end, {silent = true})
+        end,
+    },
+    {
+        'rafamadriz/friendly-snippets',
+        config = function ()
+            require("luasnip.loaders.from_vscode").lazy_load()
+            local ls = require('luasnip')
+            ls.filetype_extend('java', { 'java-tests', 'java', 'javadoc'})
+        end
+    }
 }
