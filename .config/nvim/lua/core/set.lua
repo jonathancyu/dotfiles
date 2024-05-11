b4ee1d479ef836b7af7cdb434288a16d5628e5fb
Jonathan Yu <yucjonathan@gmail.com>
2024-05-11 15:31:18 -0500

    simplified conf

--- .config/nvim/lua/core/set.lua
+++ .config/nvim/lua/core/set.lua
@@ -1,46 +1,50 @@
+local opt = vim.opt
 -- Tab configuration
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
 
 -- Use system clipboard
-vim.opt.clipboard = 'unnamedplus'
+opt.clipboard = 'unnamedplus'
 
 -- Smart indent
-vim.opt.smartindent = true
+opt.smartindent = true
+
+-- Smart delete
+opt.backspace = "indent,eol,start"
 
 -- Line numbers
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
+opt.nu = true
+opt.relativenumber = true
+opt.cursorline = true
 
 -- Word wrap and view options
-vim.opt.wrap = false
-vim.opt.scrolloff = 8
+opt.wrap = false
+opt.scrolloff = 8
 
 -- File system
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/.undodir"
-vim.opt.isfname:append("@-@")
+opt.swapfile = false
+opt.backup = false
+opt.undofile = true
+opt.undodir = os.getenv("HOME") .. "/.vim/.undodir"
+opt.isfname:append("@-@")
 
 
 -- Search configuration
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+opt.hlsearch = false
+opt.incsearch = true
 
 -- Split normally
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+opt.splitbelow = true
+opt.splitright = true
 
 -- Colors
-vim.opt.termguicolors = true
-vim.opt.colorcolumn = "120"
+opt.termguicolors = true
+opt.colorcolumn = "120"
 
 -- Disable mouse interaction
--- vim.opt.mouse = ""
+-- opt.mouse = ""
 
 -- Better netrw
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro nocursorline'
@@ -53,5 +57,5 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Other
-vim.opt.updatetime = 250
+opt.updatetime = 250
 
