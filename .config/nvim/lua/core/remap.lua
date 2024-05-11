44c29c52593212c37eaab5ac973fec468ff776e5
Jonathan Yu <yucjonathan@gmail.com>
2024-05-11 16:08:44 -0500

    feat: moved tmux-sessionizer into a tmux keybinding

--- .config/nvim/lua/core/remap.lua
+++ .config/nvim/lua/core/remap.lua
@@ -18,9 +18,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- tmux sessionizer
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-
 -- Easy pane navigation
 vim.keymap.set('n', '<C-c>', '<C-w>c')
 vim.keymap.set('i', '<C-c>', '<Esc><C-w>c')
