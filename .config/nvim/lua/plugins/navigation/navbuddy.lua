e3b15f3490f3ac6acc44fedfdf8a409224f9d550
Jonathan Yu <yucjonathan@gmail.com>
2024-06-11 22:18:15 -0500

    feat: added plugins

---
+++ .config/nvim/lua/plugins/navigation/navbuddy.lua
@@ -0,0 +1,11 @@
+return {
+    'SmiteshP/nvim-navbuddy',
+    dependencies = {
+        'SmiteshP/nvim-navic',
+        'MunifTanjim/nui.nvim'
+    },
+    opts = { lsp = { auto_attach = true } },
+    keys = {
+        { '<leader>m', '<cmd>Navbuddy<cr>' }
+    }
+}
