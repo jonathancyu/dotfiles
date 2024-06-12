d3c40f2d625d08c512a067bc5f2562d635b287ee
Jonathan Yu <yucjonathan@gmail.com>
2024-06-11 22:24:01 -0500

    upgraded trouble

--- .config/nvim/lua/plugins/navigation/trouble.lua
+++ .config/nvim/lua/plugins/navigation/trouble.lua
@@ -1,28 +1,37 @@
 return {
-    -- Lists all problems
-    'folke/trouble.nvim',
-    branch = 'dev',
-    opts = {},
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
     keys = {
         {
-            '<leader>to',
-            '<cmd>Trouble diagnostics toggle<cr>'
-        }
-
-    }
-
-    -- config = function ()
-    --     local actions = require("telescope.actions")
-    --     local trouble = require("trouble.providers.telescope")
-    --     local telescope = require("telescope")
-    --     telescope.setup {
-    --         defaults = {
-    --             mappings = {
-    --                 i = { ["<c-t>"] = trouble.open_with_trouble },
-    --                 n = { ["<c-t>"] = trouble.open_with_trouble },
-    --             },
-    --         },
-    --     }
-    -- end,
-
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+    },
 }
