0e6ac199e071d6f6813989ad2850db88f3fed863
Jonathan Yu <yucjonathan@gmail.com>
2024-06-29 18:18:03 -0500

    replaced indentblankline

--- .config/nvim/lua/plugins/editor/mini.lua
+++ .config/nvim/lua/plugins/editor/mini.lua
@@ -3,5 +3,14 @@ return {
 	config = function()
 		-- require('mini.ai').setup { n_lines = 500 }
 		require('mini.surround').setup()
+		local indentscope = require('mini.indentscope')
+		indentscope.setup({
+			symbol = '┃',
+			draw = {
+				delay = 50,
+				animation = indentscope.gen_animation.none(),
+			},
+			mappings = {},
+		})
 	end,
 }
