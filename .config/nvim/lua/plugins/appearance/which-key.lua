522f974b350249ca611011bb2980dba00aaa0637
Jonathan Yu <yucjonathan@gmail.com>
2024-07-12 23:12:22 -0500

    feat: which-key 3

--- .config/nvim/lua/plugins/appearance/which-key.lua
+++ .config/nvim/lua/plugins/appearance/which-key.lua
@@ -6,21 +6,17 @@ return { -- Useful plugin to show you pending keybinds.
 		vim.o.timeoutlen = 300
 		require('which-key').setup()
 
-		-- Document existing key chains
-		require('which-key').register({
-			['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-			['<leader>d'] = { name = '[D]ebug', _ = 'which_key_ignore' },
-			['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-			['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-			['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-			['<leader>t'] = { name = '[T]est', _ = 'which_key_ignore' },
-			['<leader>r'] = { name = '[R]un', _ = 'which_key_ignore' },
-			['<leader>w'] = { name = '[W]indow', _ = 'which_key_ignore' },
-			['<leader>x'] = { name = 'Trouble', _ = 'which_key_ignore' },
+		require('which-key').add({
+			{ '<leader>c', desc = '[C]ode' },
+			{ '<leader>d', desc = '[D]ebug' },
+			{ '<leader>g', desc = '[G]it' },
+			{ '<leader>h', desc = 'Git [H]unk' },
+			{ '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+			{ '<leader>s', desc = '[S]earch' },
+			{ '<leader>t', desc = '[T]est' },
+			{ '<leader>r', desc = '[R]un' },
+			{ '<leader>w', desc = '[W]indow' },
+			{ '<leader>x', desc = 'Trouble' },
 		})
-		-- visual mode
-		require('which-key').register({
-			['<leader>h'] = { 'Git [H]unk' },
-		}, { mode = 'v' })
 	end,
 }
