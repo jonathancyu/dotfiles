654f973a1cce4e233c78f1dd456029266b1c7799
Jonathan Yu <yucjonathan@gmail.com>
2024-07-29 22:53:42 -0500

    added rust explain

--- .config/nvim/after/ftplugin/rust.lua
+++ .config/nvim/after/ftplugin/rust.lua
@@ -1,8 +1,13 @@
 local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set('n', '<leader>ca', function()
+vim.keymap.set('n', '<leader>a', function()
 	vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
 	-- or vim.lsp.buf.codeAction() if you don't want grouping.
-end, { desc = '[C]ode [A]ction', silent = true, buffer = bufnr })
+end, { desc = '[A]ction', silent = true, buffer = bufnr })
+
+vim.keymap.set('n', '<leader>cx', function()
+	vim.cmd.RustLsp('explainError') -- supports rust-analyzer's grouping
+	-- or vim.lsp.buf.codeAction() if you don't want grouping.
+end, { desc = 'E[x]plain error', silent = true, buffer = bufnr })
 
 vim.keymap.set('n', '<leader>rd', function()
 	vim.cmd.RustLsp('debuggables')
