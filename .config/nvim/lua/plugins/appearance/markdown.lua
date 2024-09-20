e9210b102b824f1878d311581a84e7abfc5ec6b8
Jonathan Yu <yucjonathan@gmail.com>
2024-09-20 15:49:25 -0500

    reorganized markdown plugins

---
+++ .config/nvim/lua/plugins/appearance/markdown.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		'OXY2DEV/markview.nvim',
+		lazy = false,
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter',
+			'nvim-tree/nvim-web-devicons',
+		},
+	},
+	{
+		'epilande/checkbox-cycle.nvim',
+		ft = 'markdown',
+		opts = {
+			states = { '[ ]', '[/]', '[x]', '[~]' },
+		},
+		keys = {
+			{
+				'<CR>',
+				'<Cmd>CheckboxCycleNext<CR>',
+				desc = 'Checkbox Next',
+				ft = { 'markdown' },
+				mode = { 'n', 'v' },
+			},
+			{
+				'<S-CR>',
+				'<Cmd>CheckboxCyclePrev<CR>',
+				desc = 'Checkbox Previous',
+				ft = { 'markdown' },
+				mode = { 'n', 'v' },
+			},
+		},
+	},
+}
