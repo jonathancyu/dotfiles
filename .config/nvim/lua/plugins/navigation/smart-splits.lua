42ba45b1898c246afedb75c29897ea1780542cea
Jonathan Yu <yucjonathan@gmail.com>
2024-11-27 22:15:51 -0600

    feat: tmux nav -> smart splits, udpated aero bindings

---
+++ .config/nvim/lua/plugins/navigation/smart-splits.lua
@@ -0,0 +1,17 @@
+local resize_amount = 10
+return {
+	'mrjones2014/smart-splits.nvim',
+	lazy = false,
+	opts = {},
+	-- stylua: ignore
+	keys = {
+		{ '<A-h>', function() require('smart-splits').resize_left(resize_amount) end, { 'n' } },
+		{ '<A-j>', function() require('smart-splits').resize_down(resize_amount) end, { 'n' } },
+		{ '<A-k>', function() require('smart-splits').resize_up(resize_amount) end, { 'n' } },
+		{ '<A-l>', function() require('smart-splits').resize_right(resize_amount) end, { 'n' } },
+		{ '<C-h>', function() require('smart-splits').move_cursor_left() end, { 'n' } },
+		{ '<C-j>', function() require('smart-splits').move_cursor_down() end, { 'n' } },
+		{ '<C-k>', function() require('smart-splits').move_cursor_up() end, { 'n' } },
+		{ '<C-l>', function() require('smart-splits').move_cursor_right() end, { 'n' } },
+	},
+}
