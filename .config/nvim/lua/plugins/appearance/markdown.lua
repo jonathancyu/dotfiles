d8f0f02bde4b1417bb5b5c9e3bd48cb7f5073a60
Jonathan Yu <yucjonathan@gmail.com>
2024-12-07 13:50:45 -0600

    disable checkbox cycle

--- .config/nvim/lua/plugins/appearance/markdown.lua
+++ .config/nvim/lua/plugins/appearance/markdown.lua
@@ -8,26 +8,26 @@ return {
 		},
 	},
 	{
-		'epilande/checkbox-cycle.nvim',
-		ft = 'markdown',
-		opts = {
-			states = { '[ ]', '[/]', '[x]', '[~]' },
-		},
-		keys = {
-			{
-				'<CR>',
-				'<Cmd>CheckboxCycleNext<CR>',
-				desc = 'Checkbox Next',
-				ft = { 'markdown' },
-				mode = { 'n', 'v' },
-			},
-			{
-				'<S-CR>',
-				'<Cmd>CheckboxCyclePrev<CR>',
-				desc = 'Checkbox Previous',
-				ft = { 'markdown' },
-				mode = { 'n', 'v' },
-			},
-		},
+		-- 'epilande/checkbox-cycle.nvim',
+		-- ft = 'markdown',
+		-- opts = {
+		-- 	states = { '[ ]', '[/]', '[x]', '[~]' },
+		-- },
+		-- keys = {
+		-- 	{
+		-- 		'<CR>',
+		-- 		'<Cmd>CheckboxCycleNext<CR>',
+		-- 		desc = 'Checkbox Next',
+		-- 		ft = { 'markdown' },
+		-- 		mode = { 'n', 'v' },
+		-- 	},
+		-- 	{
+		-- 		'<S-CR>',
+		-- 		'<Cmd>CheckboxCyclePrev<CR>',
+		-- 		desc = 'Checkbox Previous',
+		-- 		ft = { 'markdown' },
+		-- 		mode = { 'n', 'v' },
+		-- 	},
+		-- },
 	},
 }
