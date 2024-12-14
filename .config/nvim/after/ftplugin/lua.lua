55c2e5970e8c84c30069ab43509ab00094b65f9a
Jonathan Yu <yucjonathan@gmail.com>
2024-12-13 19:03:13 -0600

    feat: easy sourcing lua

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -1,1 +1,11 @@
 vim.g.disable_autoformat = false
+require('which-key').add({
+	mode = { 'n', 'x' },
+	'<CR>',
+	function(self)
+		local buf = vim.api.nvim_get_current_buf()
+		local bufname = vim.api.nvim_buf_get_name(buf)
+		Snacks.debug.run({ buf = buf, name = bufname })
+	end,
+	desc = 'Source buffer',
+})
