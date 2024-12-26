9dd8cf26e5416a05694754f10402c8a0c83dc312
Jonathan Yu <yucjonathan@gmail.com>
2024-12-26 15:06:14 -0600

    refactor: relocated ufo

---
+++ .config/nvim/lua/plugins/appearance/ufo.lua
@@ -0,0 +1,10 @@
+return {
+	'kevinhwang91/nvim-ufo',
+	dependencies = 'kevinhwang91/promise-async',
+	opts = {
+		open_fold_hl_timeout = 50,
+		provider_selector = function()
+			return { 'treesitter', 'indent' }
+		end,
+	},
+}
--- .config/nvim/lua/plugins/lsp/treesitter.lua
+++ .config/nvim/lua/plugins/lsp/treesitter.lua
@@ -71,13 +71,4 @@ return {
 		'nvim-treesitter/nvim-treesitter-context',
 		opts = { max_lines = 3 },
 	},
-	{
-		'kevinhwang91/nvim-ufo', dependencies = 'kevinhwang91/promise-async',
-		opts = {
-
-			provider_selector = function()
-				return { 'treesitter', 'indent' }
-			end,
-		},
-	},
 }
