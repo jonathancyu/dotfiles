4c2a8aaf1e6b94bb710e5cf1c5f4c4bec21ecb92
Jonathan Yu <yucjonathan@gmail.com>
2025-03-14 11:42:53 -0500

    feat: cmdline source

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,11 +1,34 @@
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    "hrsh7th/cmp-cmdline",
+  },
   opts = {
     keymap = {
       ["<Tab>"] = { "select_and_accept" },
     },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
     cmdline = {
       enabled = true,
+      completion = {
+        menu = {
+          auto_show = true,
+        },
+      },
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        -- Search forward and backward
+        if type == "/" or type == "?" then
+          return { "buffer" }
+        end
+        -- Commands
+        if type == ":" or type == "@" then
+          return { "cmdline" }
+        end
+        return {}
+      end,
     },
   },
 }
