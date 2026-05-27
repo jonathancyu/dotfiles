975821c621dbb800daeabfb11d2acb0747424921
Jonathan Yu <jonathan@8090.ai>
2026-05-27 11:57:24 -0700

    fix: lsp cnfig

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -8,34 +8,57 @@ return {
     },
     opts = function(_, opts)
       opts.servers = opts.servers or {}
-      opts.servers = {
-        ["*"] = {
-          keys = {
-            -- Disable prev/next reference
-            { "<a-n>", false },
-            { "<a-p>", false },
-            { "<leader>ca", false },
-            -- Replace lazyvim code action
-            {
-              "<M-.>",
-              function()
-                require("fastaction").code_action()
-              end,
-              desc = "Code Action",
-              mode = { "n", "v" },
-              has = "codeAction",
-            },
-            { "<leader>cr", false },
-            {
-              "<leader>rn",
-              vim.lsp.buf.rename,
-              desc = "Rename",
-              has = "rename",
+      opts.servers["*"] = opts.servers["*"] or {}
+      opts.servers["*"].keys = vim.list_extend(opts.servers["*"].keys or {}, {
+        -- Disable prev/next reference
+        { "<a-n>", false },
+        { "<a-p>", false },
+        { "<leader>ca", false },
+        -- Replace lazyvim code action
+        {
+          "<M-.>",
+          function()
+            require("fastaction").code_action()
+          end,
+          desc = "Code Action",
+          mode = { "n", "v" },
+          has = "codeAction",
+        },
+        { "<leader>cr", false },
+        {
+          "<leader>rn",
+          vim.lsp.buf.rename,
+          desc = "Rename",
+          has = "rename",
+        },
+      })
+      opts.servers.wgsl_analyzer = opts.servers.wgsl_analyzer or {}
+
+      local python_root_markers = {
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+        "Pipfile",
+        "pyrightconfig.json",
+        ".git",
+      }
+      opts.servers.pyright = vim.tbl_deep_extend("force", opts.servers.pyright or {}, {
+        root_markers = python_root_markers,
+        settings = {
+          python = {
+            pythonPath = ".venv/bin/python",
+            venv = ".venv",
+            venvPath = ".",
+            analysis = {
+              typeCheckingMode = "strict",
             },
           },
         },
-      }
-      opts.servers.wgsl_analyzer = {}
+      })
+      opts.servers.ruff = vim.tbl_deep_extend("force", opts.servers.ruff or {}, {
+        root_markers = python_root_markers,
+      })
     end,
   },
 }
